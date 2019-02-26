ae3739d6e6d1ee64e38df37a540d7568b9303327
kwhychange <favian0710@gmail.com>
2019-02-25 16:47:50 -0800

    add Utils and testing of files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        String data = Utils.readFileAsString("data/Unemployment.csv");
+        System.out.println(data);
     }
 }
