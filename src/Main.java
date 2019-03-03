595cfda91e32aca68900f96116ca4d0e60afb31c
kwhychange <favian0710@gmail.com>
2019-03-02 16:37:20 -0800

    finished ElectionResult

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -18,4 +19,26 @@ public class Utils {
 
         return output.toString();
     }
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> results = new ArrayList<ElectionResult>();
+
+        String[] individualData = data.split("\n");
+
+        for (int i = 0; i < individualData.length; i++) {
+            String fields = individualData[i].substring(individualData[i].indexOf(",") + 1);
+            String newField = deleteParentheses(fields);
+
+            //hard coding everything
+            String[] separatedFields = new
+
+            ElectionResult addNew = new ElectionResult();
+            results.add(addNew);
+        }
+        return results;
+    }
+
+    private static String deleteParentheses(String fields) {
+    }
+
 }
